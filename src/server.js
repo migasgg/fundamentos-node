9df22c50024f78746b22e7eb12730976d81bafc7
Miguel Albert <miguelalbert38@gmail.com>
2024-12-10 21:10:11 -0300

    first commit firsts aplications on node

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from "node:http"
+
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users))
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@exemple.com",
+    })
+
+    return res.end("Criação de usuário")
+  }
+
+  return res.end("Sua aplicação falhou")
+})
+
+server.listen(3333)
